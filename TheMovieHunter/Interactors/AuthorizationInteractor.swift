cc45eb0e37a2475356d16b32a7154d37c051e20b
ws-013-11b <ivanov.stuff@gmail.com>
2018-08-27 13:37:46 +0300

    Implementing Viper

---
+++ TheMovieHunter/Interactors/AuthorizationInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  AuthorizationInteractor.swift
+//  TheMovieHunter
+//
+//  Created by ws-013-11b on 8/24/18.
+//  Copyright © 2018 ws-013-11b. All rights reserved.
+//
+
+import Foundation
---
+++ TheMovieHunter/Protocols/AuthorizationProtocols.swift
@@ -0,0 +1,14 @@
+//
+//  AuthorizationProtocols.swift
+//  TheMovieHunter
+//
+//  Created by ws-013-11b on 8/24/18.
+//  Copyright © 2018 ws-013-11b. All rights reserved.
+//
+
+import Foundation
+
+protocol AuthorizationPresetnerProtocol {
+    
+    func isRememberMe() -> Bool
+}
